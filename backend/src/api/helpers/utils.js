7cb86fd452cf5a05f427e7e0fb1ee70228b7b134
Abishek Bardewa <way2abishek@gmail.com>
2024-10-20 10:18:01 +0530

    Updates on chart color pallets

--- backend/src/api/helpers/utils.js
+++ backend/src/api/helpers/utils.js
@@ -1,6 +1,25 @@
 export const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const baseColors = ['#fd7f6f', '#7eb0d5', '#b2e061', '#bd7ebe', '#ffb55a', '#ffee65', '#beb9db', '#fdcce5', '#8bd3c7'];
+const baseColors = [
+	'#fd7f6f',
+	'#7eb0d5',
+	'#b2e061',
+	'#bd7ebe',
+	'#ffb55a',
+	'#ffee65',
+	'#beb9db',
+	'#fdcce5',
+	'#8bd3c7',
+	'#ea5545',
+	'#f46a9b',
+	'#ef9b20',
+	'#edbf33',
+	'#ede15b',
+	'#bdcf32',
+	'#87bc45',
+	'#27aeef',
+	'#b33dc6',
+];
 
 export const getBaseColor = (index) => {
 	return baseColors[index % baseColors.length];
